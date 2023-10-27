dd53111e53dd33fd2db57d499323d66c789aede4
Andrew <andrewfz831@gmail.com>
2023-10-27 16:24:06 -0700

    Connected routes in pages

--- clientSide/src/App.tsx
+++ clientSide/src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react'; // Make sure to import React if you haven't already
+import React from 'react';
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom' 
+import Homepage from './views/homePage';
+import CreatePost from './views/createPost';
+import Explore from './views/explore';
+import Favorites from './views/favorites';
+import Login from './views/login';
+import MyPost from './views/myPost';
+import SignUp from './views/signUp';
+import ViewPost from './views/viewPost';
 
 function App() {
   return (
     <>
-      <h1 className='text-red-500'>hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/CreatePost' element={<CreatePost/>}/>
+        <Route path='/Explore' element={<Explore/>}/>
+        <Route path='/Favorites' element={<Favorites/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/MyPost' element={<MyPost/>}/>
+        <Route path='/SignUp' element={<SignUp/>}/>
+        <Route path='/ViewPost' element={<ViewPost/>}/>
+      </Routes>
+    </BrowserRouter>
+   </>
   );
 }
 
