2f8cfdf677188caee41acd34ed5bd140a3f57a2d
DannyIGuesss <danielgarcia448353@gmail.com>
2023-11-17 12:34:04 -0800

    worked on server backend

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
+const db = require('./config/mysql.config')
+
 app.get('/', (req,res) => {
     return res.json(
         "From the Backend"
     )
+})
+
+app.get('/users', (res,req) => {
+    const getAllUsers = "SELECT * FROM userTable";
+
 })
 
 app.listen(8080, () => {
