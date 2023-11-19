5a931de537eb596c943371c7f012377badc4579b
DannyIGuesss <danielgarcia448353@gmail.com>
2023-11-19 15:16:32 -0800

    fixed the connection to the database

--- server/config/mysql.config.js
+++ server/config/mysql.config.js
@@ -1,19 +1,26 @@
 // This is where we make our database connection and we put it in a different file for seperation of concerns.
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
+// we are bringing in the variables from the .env file 
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "GearGrade"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
 
+// Connect to the MySQL server
 db.connect((err) => {
     if (err) {
-        console.error("error line 14", err)
-        return
-    }
-    console.log("connected to DB");
-})
+    console.error('Error connecting to MySQL:', err);
+    return;
+}
+    console.log('Connected to MySQL');
+});
+
+module.exports = db;
+
+
 console.log('config file');
